0986ac1976357e3b2819646edcb969f2f8ac0ad1
Laurent Direr <laurent.direr@gmail.com>
2014-10-18 23:12:37 +0200

    Added functions to compute cycle time.

--- post_processing.py
+++ post_processing.py
@@ -1,9 +1,10 @@
 from datetime import time
+import numpy as np
+import pandas as pd
 
-x = excel_time # a float
-x = int(x * 24 * 3600 * 1000) # convert to number of microseconds
-
-my_time = time(x // (3600 * 1000), 
-(x % (3600 * 1000))//(60 * 1000), 
-x % (3600 * 60 * 1000),
-x ) # hours, minutes, seconds
+def xls_to_seconds(t):
+    return t * 24 * 3600
+for light_id in data.traffic_light_id.drop_duplicates():
+    print light_id, ': ', xls_to_seconds(np.mean(data.loc[(data['traffic_light_id'] == light_id) & (data['changed_state'] == 3), 'created_at'].diff()))
+for light_id in data.traffic_light_id.drop_duplicates():
+    print light_id, ': ', xls_to_seconds(np.mean(data.loc[(data['traffic_light_id'] == light_id) & (data['changed_state'] == 1), 'created_at'].diff()))
